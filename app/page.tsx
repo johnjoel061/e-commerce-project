3b3cf9caf2422dcd4cec1bbbd3d8d04a43e2035e
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-04-29 18:34:12 +0800

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <main className="flex items-center justify-center min-h-screen bg-blue-2">
+      <h1 className="text-heading1-bold text-blue-1">Hello Tailwind + Next.js</h1>
+    </main>
   );
 }
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
-
 export default config;
