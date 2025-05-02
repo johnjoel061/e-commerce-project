78c1f6073315ebdae7f3a800de8c48e1b3ace1f1
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-02 15:07:27 +0800

    ...

--- app/components/layout/TopBar.tsx
+++ app/components/layout/TopBar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { navLinks } from "@/lib/constants"
+import { UserButton } from "@clerk/nextjs"
+import { Menu } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
 
 const TopBar = () => {
   return (
-    <div>
-      TOP BAR
+    <div className="sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 b-blue-2 shadow-xl">
+      <Image src="/logo.png" alt='logo' width={150} height={70} />
+      <div className='flex gap-8 max-md:hidden'>
+        {navLinks.map((link) => (
+          <Link href={link.url} key={link.label} className='flex gap-4 text-body-medium'>{link.icon} <p>{link.label}</p></Link>
+        ))}
+      </div>
+
+      <div className='flex gap-4 items-center'>
+        <Menu className="cursor-pointer md:hidden"/>
+        <UserButton />
+      </div>
     </div>
   )
 }
