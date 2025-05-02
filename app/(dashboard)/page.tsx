024a74d66bdbc202abcf9c5164150af082c2a725
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-02 12:40:58 +0800

    ...

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-blue-2">
-      <h1 className="text-heading1-bold text-blue-1">Hello Tailwind + Next.js</h1>
-    </main>
+      <UserButton/>
   );
 }
