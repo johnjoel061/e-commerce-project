69ddae9c4265c0ac431d19d764184021149dc344
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-02 15:37:08 +0800

    ...

--- app/components/layout/LeftSideBar.tsx
+++ app/components/layout/LeftSideBar.tsx
@@ -1,24 +1,39 @@
-import { navLinks } from '@/lib/constants'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client"
+
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { navLinks } from "@/lib/constants";
 
 const LeftSideBar = () => {
-    return (
-        <div className='h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>
-            <Image src="/logo.png" alt='logo' width={150} height={70} />
-            <div className='flex flex-col gap-12'>
-                {navLinks.map((link) => (
-                    <Link href={link.url} key={link.label} className='flex gap-4 text-body-medium'>{link.icon} <p>{link.label}</p></Link>
-                ))}
-            </div>
+  const pathname = usePathname();
 
-            <div className='flex gap-4 text-body-medium items-center'>
-                <UserButton/>
-                <p>Edit Profile</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden">
+      <Image src="/logo.png" alt="logo" width={150} height={70} />
 
-export default LeftSideBar
+      <div className="flex flex-col gap-12">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className={`flex gap-4 text-body-medium ${
+              pathname === link.url ? "text-blue-1" : "text-grey-1"
+            }`}
+          >
+            {link.icon} <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+
+      <div className="flex gap-4 text-body-medium items-center">
+        <UserButton />
+        <p>Edit Profile</p>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideBar;
