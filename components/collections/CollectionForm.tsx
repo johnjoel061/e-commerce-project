f4f33e46d417bb898b8363bacb3d149b42acf184
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-03 15:26:13 +0800

    ...

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -1,10 +1,69 @@
+"use client"
+
 import { Separator } from "../ui/separator"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string()
+})
 
 const CollectionForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
   return (
     <div className='p-10'>
       <h1 className='text-heading2-bold'>Create Collection</h1>
-      <Separator className="my-4"/>
+      <Separator className="my-4" />
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   )
 }
