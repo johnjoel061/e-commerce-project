7af2bb2f3592c297230150e336a37a0190b37e12
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-03 15:09:45 +0800

    ...

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -1,8 +1,10 @@
+import { Separator } from "../ui/separator"
 
 const CollectionForm = () => {
   return (
     <div className='p-10'>
-      <p className='text-heading2-bold'>Create Collection</p>
+      <h1 className='text-heading2-bold'>Create Collection</h1>
+      <Separator className="my-4"/>
     </div>
   )
 }
