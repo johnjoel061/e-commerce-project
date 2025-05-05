7bc85f9d482bbb5b2cb5ff54cae20f4714a58425
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-05 15:22:55 +0800

    ...

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from "react"
 
 const Collections = () => {
+  const [loading, setLoading] = useState(true);
+  const [collections, setCollections] = useState([]);
+
+  const getCollections = async () => {
+    try {
+      const res = await fetch("/api/collections", {
+        method: "GET",
+      })
+      const data = await res.json()
+      setCollections(data)
+      setLoading(false)
+    } catch (error) {
+      console.log("[collections_GET]", error)
+    }
+  }
+
+  useEffect(() => {
+    getCollections()
+  }, [])
+
+  
   return (
     <div>
       Collections
