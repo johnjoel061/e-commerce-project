fc6ce0e0cf52963ccce07b1c292c939b5840b9b3
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-09 19:28:51 +0800

    ...

--- components/collections/CollectionColumns.tsx
+++ components/collections/CollectionColumns.tsx
@@ -5,18 +5,24 @@ import Delete from "../custom ui/Delete"
 import Link from "next/link"
 
 export const columns: ColumnDef<CollectionType>[] = [
-    {
-        accessorKey: "title",
-        header: "Title",
-        cell: ({ row }) => <Link href={`/collections/${row.original._id}`} className="hover:text-red-1"><p>{row.original.title}</p></Link>
-    },
-    {
-        accessorKey: "products",
-        header: "Products",
-        cell: ({ row }) => <p>{row.original.products.length}</p>
-    },
-    {
-        id: "actions",
-        cell: ({ row }) => <Delete id={row.original._id}/>,
-    },
+  {
+    accessorKey: "title",
+    header: "Title",
+    cell: ({ row }) => (
+      <Link href={`/collections/${row.original._id}`}>
+        <p className="hover:text-red-500 transition-colors duration-200">{row.original.title}</p>
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "products",
+    header: "Products",
+    cell: ({ row }) => (
+      <p>{row.original.products.length}</p>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <Delete id={row.original._id} />,
+  }
 ]
