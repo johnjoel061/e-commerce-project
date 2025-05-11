eca29aa7d94bf32d5734e1481daa613ecfe564c8
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-11 15:34:34 +0800

    ...

--- components/products/ProductsForm.tsx
+++ components/products/ProductsForm.tsx
@@ -1,11 +1,148 @@
-import React from 'react'
+"use client"
 
-const ProductsForm = () => {
+import { Separator } from "../ui/separator"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { array, z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "../ui/textarea"
+import ImageUpload from "../custom ui/ImageUpload"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
+import toast from "react-hot-toast"
+import Delete from "../custom ui/Delete"
+
+
+const formSchema = z.object({
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  media: z.array(z.string()),
+  category: z.string(),
+  collections: z.array(z.string()),
+  tags: z.array(z.string()),
+  sizes: z.array(z.string()),
+  colors: z.array(z.string()),
+  price: z.coerce.number().min(0.1),
+  expense: z.coerce.number().min(0.1),
+})
+
+interface ProductFormProps {
+  initialData?: ProductType | null;
+}
+
+const ProductForm: React.FC<ProductFormProps> = ({ initialData }) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData ? initialData : {
+      title: "",
+      description: "",
+      image: "",
+    },
+  })
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  }
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const url = initialData ? `/api/collections/${initialData._id}` : "/api/collections"
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(values)
+      })
+      if (res.ok) {
+        setLoading(false);
+        toast.success(`Collection ${initialData ? "updated" : "created"}`);
+        window.location.href = "/collections";
+        router.push(`collections`);
+      }
+    } catch (error) {
+      console.log("[collections_POST]", error);
+      toast.error("Something went wrong! Please try again.");
+    }
+  }
+
   return (
-    <div>
-      Products Form
+    <div className='p-10'>
+      {initialData ? (
+        <div className="flex items-center justify-between">
+          <h1 className='text-heading2-bold'>Edit Collection</h1>
+          <Delete id={initialData._id}/>
+        </div>
+        ) : (
+        <h1 className='text-heading2-bold'>Create Collection</h1>)}
+      <Separator className="my-4" />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" {...field} onKeyDown={handleKeyPress}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Description" {...field} rows={5} onKeyDown={handleKeyPress}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <ImageUpload 
+                    value={field.value ? [field.value] : []}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex gap-10">
+            <Button type="submit" className="bg-blue-1 text-white cursor-pointer">Submit</Button>
+            <Button type="button" onClick={() => router.push("/collections")} className="bg-blue-1 text-white cursor-pointer">Discard</Button>
+          </div>
+        </form>
+      </Form>
     </div>
   )
 }
 
-export default ProductsForm
+export default ProductForm
