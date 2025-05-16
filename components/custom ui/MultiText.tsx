a5e7ebad06c5e5fef1971202630af2aa42f3cfa5
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-16 13:22:24 +0800

    ...

--- components/custom ui/MultiText.tsx
+++ components/custom ui/MultiText.tsx
@@ -1,5 +1,9 @@
+"use client"
 import React, { useState } from 'react'
 import { Input } from '../ui/input'
+import { Badge } from '../ui/badge'
+import { Button } from '../ui/button'
+import { X } from 'lucide-react'
 
 interface MultiTextProps {
     placeholder: string
@@ -20,19 +24,31 @@ const MultiText: React.FC<MultiTextProps> = ({
         setInputValue("");
     }
 
-  return (
-    <Input
-    placeholder={placeholder}
-    value={inputValue}
-    onChange={(e) => setInputValue(e.target.value)}
-    onKeyDown={(e) => {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            addTag(inputValue)
-        }
-    }}
-    />
-  )
+    return (
+        <>
+            <Input
+                placeholder={placeholder}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        e.preventDefault();
+                        addTag(inputValue)
+                    }
+                }}
+            />
+
+            <div className='flex gap-1 flex-wrap mt-4'>
+                {value.map((tag, index) => (
+                    <Badge key={index} className='bg-grey-1 text-white'>{tag}
+                        <Button className='ml-1 rounded-full outline-none hover:bg-red-1' size="sm" onClick={() => onRemove(tag)}>
+                            <X className='h-3 w-3'/>
+                        </Button>
+                    </Badge>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default MultiText
